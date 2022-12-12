98e0619ccb3e4f411bf15ab7930aeb123e143306
Akshay Khona <DesertSled800c@gmail.com>
2022-12-12 10:04:14 -0600

    proj complete

---
+++ main.js
@@ -0,0 +1,10 @@
+const addTwo = (num1, num2) => {
+    return num1 + num2;
+}
+
+const multiplyByTwo = (obj, num2) => {
+    return obj * num2
+}
+
+let sum = addTwo(4, 14)
+console.log(multiplyByTwo(sum, 2))
